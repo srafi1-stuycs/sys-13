efac6046c99fed1be846fb64a621b838a5cac1dc
srafi1 <shakilrafi0@gmail.com>
2017-11-20 18:51:03 -0500

    donezo

---
+++ makefile
@@ -0,0 +1,6 @@
+compile: mario.c
+	gcc mario.c
+run: a.out
+	./a.out
+clean: a.out
+	rm a.out
--- mario.c
+++ mario.c
@@ -14,16 +14,28 @@ int main() {
     int f = fork();
     if (f) {
         //parent
+        close(mario[READ]);
+        close(luigi[WRITE]);
         int send = 10;
         printf("[parent] sending: %d\n", send);
         write(mario[WRITE], &send, sizeof(send));
+        close(mario[WRITE]);
         int receive;
         read(luigi[READ], &receive, sizeof(receive));
+        close(luigi[READ]);
         printf("[parent] received: %d\n", receive);
     } else {
+        //child
+        close(mario[WRITE]);
+        close(luigi[READ]);
         int receive;
         read(mario[READ], &receive, sizeof(receive));
         printf("[child] received: %d\n", receive);
+        receive += 15;
+        printf("[child] applied da mathematics: %d\n", receive);
+        write(luigi[WRITE], &receive, sizeof(receive));
+        close(mario[READ]);
+        close(luigi[WRITE]);
     }
     return 0;
 }
